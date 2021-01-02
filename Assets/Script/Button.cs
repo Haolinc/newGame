83e81042f1e1e5e0ec244509b7eadb20165eeb8a
Haolin <forskyrimdemacia@gmail.com>
2021-01-01 19:41:38 -0500

    Added button functions and GUI

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public void closeAll()
+    {
+        GameObject law = GameObject.Find("/Canvas/Buttons&Tabs/Law_Tab");
+        GameObject min = GameObject.Find("/Canvas/Buttons&Tabs/Minister_Tab");
+        GameObject dip = GameObject.Find("/Canvas/Buttons&Tabs/Diplomacy_Tab");
+        GameObject stat = GameObject.Find("/Canvas/Buttons&Tabs/Statistic_Tab");
+        GameObject menu = GameObject.Find("/Canvas/Buttons&Tabs/Menu_Tab");
+        GameObject pstatus = GameObject.Find("/Canvas/Buttons&Tabs/PlayerStatus_Tab");
+        law.SetActive(false);
+        min.SetActive(false);
+        dip.SetActive(false);
+        stat.SetActive(false);
+        menu.SetActive(false);
+        pstatus.SetActive(false);
+    }
+    public void lawOpen()
+    {
+        GameObject law = GameObject.Find("/Canvas/Buttons&Tabs/Law_Tab");
+        closeAll();
+        law.SetActive(true);
+    }
+    public void minOpen()
+    {
+        GameObject min = GameObject.Find("/Canvas/Buttons&Tabs/Minister_Tab");
+        closeAll();
+        min.SetActive(true);
+    }
+    public void dipOpen()
+    {
+        GameObject dip = GameObject.Find("/Canvas/Buttons&Tabs/Diplomacy_Tab");
+        closeAll();
+        dip.SetActive(true);
+    }
+    public void statOpen()
+    {
+        GameObject stat = GameObject.Find("/Canvas/Buttons&Tabs/Statistic_Tab");
+        closeAll();
+        stat.SetActive(true);
+    }
+    public void menuOpen()
+    {
+        GameObject menu = GameObject.Find("/Canvas/Buttons&Tabs/Menu_Tab");
+        closeAll();
+        menu.SetActive(true);
+    }
+    public void pstatusOpen()
+    {
+        GameObject pstatus = GameObject.Find("/Canvas/Buttons&Tabs/PlayerStatus_Tab");
+        closeAll();
+        pstatus.SetActive(true);
+    }
+}
