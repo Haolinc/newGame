6316b7fdaef53686a72ab847a95806a972f66eb9
Haolin <forskyrimdemacia@gmail.com>
2021-01-03 13:53:48 -0500

    Added display value script

---
+++ Assets/Script/DisplayValues.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayValues : MonoBehaviour
+{
+    Values value;
+    Text authority;
+    // Start is called before the first frame update
+    void Start()
+    {
+        authority = GameObject.Find("/Canvas/Values/Authority").GetComponent<Text>();
+        value = GameObject.Find("ValueHolder").GetComponent<Values>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        authority.text = "皇帝权威： " + value.val["Authority"].ToString();
+    }
+}
